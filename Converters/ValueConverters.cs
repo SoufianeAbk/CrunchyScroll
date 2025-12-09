78f6499ebad8bc91eb12c9d06d3083cf5006c022
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-12-09 19:27:07 +0100

    Value Converter Implementation.

---
+++ Converters/ValueConverters.cs
@@ -0,0 +1,87 @@
+﻿using System.Globalization;
+using CrunchyScroll.Models;
+
+namespace CrunchyScroll.Converters
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
+    }
+
+    public class IsNotZeroConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+                return intValue > 0;
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is OrderStatus status)
+            {
+                return status switch
+                {
+                    OrderStatus.Pending => Color.FromArgb("#FFA500"),      // Orange
+                    OrderStatus.Processing => Color.FromArgb("#0000FF"),   // Blue
+                    OrderStatus.Shipped => Color.FromArgb("#800080"),      // Purple
+                    OrderStatus.Delivered => Color.FromArgb("#008000"),    // Green
+                    OrderStatus.Cancelled => Color.FromArgb("#FF0000"),    // Red
+                    _ => Color.FromArgb("#808080")                         // Gray
+                };
+            }
+            return Color.FromArgb("#808080");
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StatusToTextConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is OrderStatus status)
+            {
+                return status switch
+                {
+                    OrderStatus.Pending => "In behandeling",
+                    OrderStatus.Processing => "Wordt verwerkt",
+                    OrderStatus.Shipped => "Onderweg",
+                    OrderStatus.Delivered => "Afgeleverd",
+                    OrderStatus.Cancelled => "Geannuleerd",
+                    _ => status.ToString()
+                };
+            }
+            return value?.ToString() ?? "";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
