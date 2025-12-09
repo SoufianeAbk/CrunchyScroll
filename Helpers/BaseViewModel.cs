f6de09a26ee71f5c252b639de17976ac26fbaac1
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-12-09 19:29:03 +0100

    Helpers Implementation.

--- Helpers/BaseViewModel.cs
+++ Helpers/BaseViewModel.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CrunchyScroll.Helpers
 {
-    internal class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
+        private bool _isBusy;
+        private string _title = string.Empty;
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                if (_isBusy != value)
+                {
+                    _isBusy = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(IsNotBusy));
+                }
+            }
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
